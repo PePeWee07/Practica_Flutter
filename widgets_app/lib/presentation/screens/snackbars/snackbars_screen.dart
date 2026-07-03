242fcff6822176d39dc30f21caf2b90a94548c89
PePeWee07 <pepewee07@gmail.com>
2026-07-02 21:44:32 -0500

    feat: Agregar nuevas pantallas: AnimatedScreen, AppTutorialScreen, InfiniteScrollScreen, ProgressScreen, SnackbarsScreen y UiControlsScreen

---
+++ widgets_app/lib/presentation/screens/snackbars/snackbars_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SnackbarsScreen extends StatelessWidget {
+  const SnackbarsScreen({super.key});
+
+  static const name = 'snackbar_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

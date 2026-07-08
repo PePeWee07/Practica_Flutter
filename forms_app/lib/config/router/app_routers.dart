09fdcbacb6f1b7e3285b0a67221b9303e696be30
PePeWee07 <pepewee07@gmail.com>
2026-07-08 09:18:07 -0500

    Base structure

---
+++ forms_app/lib/config/router/app_routers.dart
@@ -0,0 +1,12 @@
+import 'package:forms_app/presentation/screens/bloc_counter_screen.dart';
+import 'package:forms_app/presentation/screens/cubit_counter_screen.dart';
+import 'package:forms_app/presentation/screens/home_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouer = GoRouter(
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
+    GoRoute(path: '/cubits', builder: (context, state) => const CubirCounterScreen()),
+    GoRoute(path: '/bloc', builder: (context, state) => const BlocCounterScreen()),
+  ],
+);
